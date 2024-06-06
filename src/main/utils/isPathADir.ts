6bc7e7477dccc417011c6d677d9ee740b96622c7
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2024-06-05 23:35:59 -0300

    add try catch block so the function doesn't throw with broken symlinks

--- src/main/utils/isPathADir.ts
+++ src/main/utils/isPathADir.ts
@@ -2,29 +2,34 @@ import path from 'path';
 import fs, { Dirent } from 'fs';
 
 const isPathADir = (pathOrDir: string | Dirent) => {
-  if (typeof pathOrDir === 'string') {
-    const stat = fs.statSync(pathOrDir);
+  try {
+    if (typeof pathOrDir === 'string') {
+      const stat = fs.statSync(pathOrDir);
 
-    if (stat.isDirectory()) return true;
+      if (stat.isDirectory()) return true;
 
-    if (!stat.isSymbolicLink()) return false;
+      if (!stat.isSymbolicLink()) return false;
 
-    const symlinkTarget = fs.readlinkSync(pathOrDir);
+      const symlinkTarget = fs.readlinkSync(pathOrDir);
 
-    const symlinkStat = fs.statSync(symlinkTarget);
+      const symlinkStat = fs.statSync(symlinkTarget);
+
+      return symlinkStat.isDirectory();
+    }
+
+    if (pathOrDir.isDirectory()) return true;
+
+    if (!pathOrDir.isSymbolicLink()) return false;
+
+    const symlinkPath = path.join(pathOrDir.path, pathOrDir.name);
+
+    const symlinkStat = fs.statSync(symlinkPath);
 
     return symlinkStat.isDirectory();
+  } catch {
+    return false;
   }
-
-  if (pathOrDir.isDirectory()) return true;
-
-  if (!pathOrDir.isSymbolicLink()) return false;
-
-  const symlinkPath = path.join(pathOrDir.path, pathOrDir.name);
-
-  const symlinkStat = fs.statSync(symlinkPath);
-
-  return symlinkStat.isDirectory();
 };
 
 export default isPathADir;
+
