3b9a8788fe0c82a7e9a34ca489172aad73c81c3f
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2024-06-05 23:34:42 -0300

    fix misstype

--- src/main/utils/isPathADir.ts
+++ src/main/utils/isPathADir.ts
@@ -1,26 +1,26 @@
 import path from 'path';
 import fs, { Dirent } from 'fs';
 
-const isPathADir = (pathForDir: string | Dirent) => {
-  if (typeof pathForDir === 'string') {
-    const stat = fs.statSync(pathForDir);
+const isPathADir = (pathOrDir: string | Dirent) => {
+  if (typeof pathOrDir === 'string') {
+    const stat = fs.statSync(pathOrDir);
 
     if (stat.isDirectory()) return true;
 
     if (!stat.isSymbolicLink()) return false;
 
-    const symlinkTarget = fs.readlinkSync(pathForDir);
+    const symlinkTarget = fs.readlinkSync(pathOrDir);
 
     const symlinkStat = fs.statSync(symlinkTarget);
 
     return symlinkStat.isDirectory();
   }
 
-  if (pathForDir.isDirectory()) return true;
+  if (pathOrDir.isDirectory()) return true;
 
-  if (!pathForDir.isSymbolicLink()) return false;
+  if (!pathOrDir.isSymbolicLink()) return false;
 
-  const symlinkPath = path.join(pathForDir.path, pathForDir.name);
+  const symlinkPath = path.join(pathOrDir.path, pathOrDir.name);
 
   const symlinkStat = fs.statSync(symlinkPath);
 
