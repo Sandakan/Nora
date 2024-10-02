71f28b0601838be75b5f9a83d742094ad33e7c80
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2024-10-03 00:41:11 +0700

    Update discordRPC.ts
    Prevent spamming Discord RPC server

--- src/main/other/discordRPC.ts
+++ src/main/other/discordRPC.ts
@@ -2,6 +2,8 @@ import { getUserData } from '../filesystem';
 import log from '../log';
 import { Initialize, setDiscordRPC } from './discord';
 
+var dataQueue: any[] = [];
+
 export const setDiscordRpcActivity = (data: any) => {
   try {
     const userData = getUserData();
@@ -12,8 +14,20 @@ export const setDiscordRpcActivity = (data: any) => {
         reason: { enableDiscordRPC }
       });
     Initialize();
-    setDiscordRPC(data);
-    return log('Discord rich presence activity accepted.');
+    if (dataQueue.length == 0) {
+      setDiscordRPC(data);
+      setTimeout(() => {
+        if (dataQueue.length > 1) {
+          log('Send last activity in the queue.');
+          setDiscordRPC(dataQueue.pop());
+        }
+        log('Clear activity queue.');
+        dataQueue = [];
+      }, 3000);
+      log('Discord rich presence activity accepted.');
+    }
+    dataQueue.push(data);
+    return log('Pushed activity to queue.');
   } catch (error) {
     return log('Failed to set discord rich presence activity.', { error });
   }
