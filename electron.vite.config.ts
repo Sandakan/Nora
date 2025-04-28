cd57eb1914c0c9cac0a5a3403a0adf310aed9a9f
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2025-04-28 20:57:40 +0530

    Fixed electron.vite incorrect config with tanstack router

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -37,13 +37,13 @@ export default defineConfig({
       }
     },
     plugins: [
-      react(),
-      tailwindcss(),
       TanStackRouterVite({
         target: 'react',
         routesDirectory: 'src/renderer/src/routes',
         autoCodeSplitting: true
-      })
+      }),
+      react(),
+      tailwindcss(),
     ]
   }
 });
