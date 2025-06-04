0a995d47096050bb87ad6a12d810ad5027567a70
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2025-06-04 10:06:14 +0530

    Refactor database schema and update SQL migration scripts
    - Updated the `schema.ts` file to introduce new enums for artwork sources and swatch types.
    - Added new tables: `artists`, `songs`, `artworks`, `palettes`, `play_events`, `seek_events`, `skip_events`, and their respective relationships.
    - Removed deprecated fields and adjusted primary keys and foreign key constraints for better normalization.
    - Created a new SQL migration file to reflect the updated schema, including type definitions and table structures.
    - Ensured all foreign key relationships are properly defined to maintain data integrity.

---
+++ resources/drizzle/0000_cuddly_warbound.sql
@@ -0,0 +1,184 @@
+CREATE TYPE "public"."artwork_source" AS ENUM('LOCAL', 'REMOTE');--> statement-breakpoint
+CREATE TYPE "public"."swatch_type" AS ENUM('VIBRANT', 'LIGHT_VIBRANT', 'DARK_VIBRANT', 'MUTED', 'LIGHT_MUTED', 'DARK_MUTED');--> statement-breakpoint
+CREATE TABLE "album_artists" (
+	"album_id" integer NOT NULL,
+	"artist_id" integer NOT NULL,
+	CONSTRAINT "album_artists_album_id_artist_id_pk" PRIMARY KEY("album_id","artist_id")
+);
+--> statement-breakpoint
+CREATE TABLE "album_songs" (
+	"album_id" integer NOT NULL,
+	"song_id" integer NOT NULL,
+	CONSTRAINT "album_songs_album_id_song_id_pk" PRIMARY KEY("album_id","song_id")
+);
+--> statement-breakpoint
+CREATE TABLE "albums" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "albums_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" varchar(255) NOT NULL,
+	"year" integer
+);
+--> statement-breakpoint
+CREATE TABLE "albums_artworks" (
+	"album_id" integer NOT NULL,
+	"artwork_id" integer NOT NULL,
+	CONSTRAINT "albums_artworks_album_id_artwork_id_pk" PRIMARY KEY("album_id","artwork_id")
+);
+--> statement-breakpoint
+CREATE TABLE "artists" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "artists_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(1024) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "artists_artworks" (
+	"artist_id" integer NOT NULL,
+	"artwork_id" integer NOT NULL,
+	CONSTRAINT "artists_artworks_artist_id_artwork_id_pk" PRIMARY KEY("artist_id","artwork_id")
+);
+--> statement-breakpoint
+CREATE TABLE "artists_songs" (
+	"song_id" integer NOT NULL,
+	"artist_id" integer NOT NULL,
+	CONSTRAINT "artists_songs_song_id_artist_id_pk" PRIMARY KEY("song_id","artist_id")
+);
+--> statement-breakpoint
+CREATE TABLE "artworks" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "artworks_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"path" text NOT NULL,
+	"source" "artwork_source" DEFAULT 'LOCAL' NOT NULL,
+	"width" integer NOT NULL,
+	"height" integer NOT NULL,
+	"palette_id" integer
+);
+--> statement-breakpoint
+CREATE TABLE "artworks_genres" (
+	"genre_id" integer NOT NULL,
+	"artwork_id" integer NOT NULL,
+	CONSTRAINT "artworks_genres_genre_id_artwork_id_pk" PRIMARY KEY("genre_id","artwork_id")
+);
+--> statement-breakpoint
+CREATE TABLE "artworks_playlists" (
+	"playlist_id" integer NOT NULL,
+	"artwork_id" integer NOT NULL,
+	CONSTRAINT "artworks_playlists_playlist_id_artwork_id_pk" PRIMARY KEY("playlist_id","artwork_id")
+);
+--> statement-breakpoint
+CREATE TABLE "artworks_songs" (
+	"song_id" integer NOT NULL,
+	"artwork_id" integer NOT NULL,
+	CONSTRAINT "artworks_songs_song_id_artwork_id_pk" PRIMARY KEY("song_id","artwork_id")
+);
+--> statement-breakpoint
+CREATE TABLE "folder_blacklist" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "folder_blacklist_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"path" text NOT NULL,
+	CONSTRAINT "folder_blacklist_path_unique" UNIQUE("path")
+);
+--> statement-breakpoint
+CREATE TABLE "genres" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "genres_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "genres_songs" (
+	"genre_id" integer NOT NULL,
+	"song_id" integer NOT NULL,
+	CONSTRAINT "genres_songs_genre_id_song_id_pk" PRIMARY KEY("genre_id","song_id")
+);
+--> statement-breakpoint
+CREATE TABLE "palette_swatches" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "palette_swatches_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"population" integer NOT NULL,
+	"hex" varchar(255) NOT NULL,
+	"hsl" varchar(255) NOT NULL,
+	"swatch_type" "swatch_type" DEFAULT 'VIBRANT' NOT NULL,
+	"palette_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "palettes" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "palettes_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"song_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "play_events" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "play_events_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"playback_percentage" numeric(5, 1) NOT NULL,
+	"song_id" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "playlists" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "playlists_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "playlists_songs" (
+	"playlist_id" integer NOT NULL,
+	"song_id" integer NOT NULL,
+	CONSTRAINT "playlists_songs_playlist_id_song_id_pk" PRIMARY KEY("playlist_id","song_id")
+);
+--> statement-breakpoint
+CREATE TABLE "seek_events" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "seek_events_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"position" numeric(8, 3) NOT NULL,
+	"song_id" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "skip_events" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "skip_events_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"position" numeric(8, 3) NOT NULL,
+	"song_id" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "song_blacklist" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "song_blacklist_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"song_id" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "song_blacklist_song_id_unique" UNIQUE("song_id")
+);
+--> statement-breakpoint
+CREATE TABLE "songs" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "songs_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" varchar(4096) NOT NULL,
+	"duration" numeric(10, 3) NOT NULL,
+	"path" text NOT NULL,
+	"sample_rate" integer,
+	"bit_rate" integer,
+	"no_of_channels" integer,
+	"file_created_at" timestamp NOT NULL,
+	"file_modified_at" timestamp NOT NULL,
+	"year" integer,
+	"disk_number" integer,
+	"track_number" integer,
+	CONSTRAINT "songs_path_unique" UNIQUE("path")
+);
+--> statement-breakpoint
+ALTER TABLE "album_artists" ADD CONSTRAINT "album_artists_album_id_albums_id_fk" FOREIGN KEY ("album_id") REFERENCES "public"."albums"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "album_artists" ADD CONSTRAINT "album_artists_artist_id_artists_id_fk" FOREIGN KEY ("artist_id") REFERENCES "public"."artists"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "album_songs" ADD CONSTRAINT "album_songs_album_id_albums_id_fk" FOREIGN KEY ("album_id") REFERENCES "public"."albums"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "album_songs" ADD CONSTRAINT "album_songs_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "albums_artworks" ADD CONSTRAINT "albums_artworks_album_id_albums_id_fk" FOREIGN KEY ("album_id") REFERENCES "public"."albums"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "albums_artworks" ADD CONSTRAINT "albums_artworks_artwork_id_artworks_id_fk" FOREIGN KEY ("artwork_id") REFERENCES "public"."artworks"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artists_artworks" ADD CONSTRAINT "artists_artworks_artist_id_artists_id_fk" FOREIGN KEY ("artist_id") REFERENCES "public"."artists"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artists_artworks" ADD CONSTRAINT "artists_artworks_artwork_id_artworks_id_fk" FOREIGN KEY ("artwork_id") REFERENCES "public"."artworks"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artists_songs" ADD CONSTRAINT "artists_songs_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artists_songs" ADD CONSTRAINT "artists_songs_artist_id_artists_id_fk" FOREIGN KEY ("artist_id") REFERENCES "public"."artists"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks" ADD CONSTRAINT "artworks_palette_id_palettes_id_fk" FOREIGN KEY ("palette_id") REFERENCES "public"."palettes"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks_genres" ADD CONSTRAINT "artworks_genres_genre_id_genres_id_fk" FOREIGN KEY ("genre_id") REFERENCES "public"."genres"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks_genres" ADD CONSTRAINT "artworks_genres_artwork_id_artworks_id_fk" FOREIGN KEY ("artwork_id") REFERENCES "public"."artworks"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks_playlists" ADD CONSTRAINT "artworks_playlists_playlist_id_playlists_id_fk" FOREIGN KEY ("playlist_id") REFERENCES "public"."playlists"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks_playlists" ADD CONSTRAINT "artworks_playlists_artwork_id_artworks_id_fk" FOREIGN KEY ("artwork_id") REFERENCES "public"."artworks"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks_songs" ADD CONSTRAINT "artworks_songs_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "artworks_songs" ADD CONSTRAINT "artworks_songs_artwork_id_artworks_id_fk" FOREIGN KEY ("artwork_id") REFERENCES "public"."artworks"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "genres_songs" ADD CONSTRAINT "genres_songs_genre_id_genres_id_fk" FOREIGN KEY ("genre_id") REFERENCES "public"."genres"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "genres_songs" ADD CONSTRAINT "genres_songs_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "palette_swatches" ADD CONSTRAINT "palette_swatches_palette_id_palettes_id_fk" FOREIGN KEY ("palette_id") REFERENCES "public"."palettes"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "palettes" ADD CONSTRAINT "palettes_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "play_events" ADD CONSTRAINT "play_events_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlists_songs" ADD CONSTRAINT "playlists_songs_playlist_id_playlists_id_fk" FOREIGN KEY ("playlist_id") REFERENCES "public"."playlists"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlists_songs" ADD CONSTRAINT "playlists_songs_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "seek_events" ADD CONSTRAINT "seek_events_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "skip_events" ADD CONSTRAINT "skip_events_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "song_blacklist" ADD CONSTRAINT "song_blacklist_song_id_songs_id_fk" FOREIGN KEY ("song_id") REFERENCES "public"."songs"("id") ON DELETE no action ON UPDATE no action;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,239 +1,279 @@
 import {
   pgTable,
-  serial,
-  text,
+  pgEnum,
+  varchar,
   integer,
   timestamp,
-  doublePrecision,
-  uniqueIndex,
+  decimal,
+  text,
   primaryKey
 } from 'drizzle-orm/pg-core';
 
-export const palettes = pgTable('palettes', {
-  palette_id: integer('palette_id').notNull().unique().primaryKey()
-});
+// Enums
+export const artworkSourceEnum = pgEnum('artwork_source', ['LOCAL', 'REMOTE']);
+export const swatchTypeEnum = pgEnum('swatch_type', [
+  'VIBRANT',
+  'LIGHT_VIBRANT',
+  'DARK_VIBRANT',
+  'MUTED',
+  'LIGHT_MUTED',
+  'DARK_MUTED'
+]);
 
-export const artworks = pgTable('artworks', {
-  artwork_id: integer('artwork_id').notNull().unique().primaryKey(),
-  file_path: text('file_path').notNull().unique()
-});
-
-export const onlineArtistArtworks = pgTable('online_artist_artworks', {
-  online_artist_artwork_id: integer('online_artist_artwork_id').notNull().unique().primaryKey(),
-  small_artwork_path: text('small_artwork_path').notNull(),
-  medium_artwork_path: text('medium_artwork_path').notNull(),
-  large_artwork_path: text('large_artwork_path')
-});
-
-export const albums = pgTable('albums', {
-  album_id: integer('album_id').notNull().unique().primaryKey(),
-  title: text('title').notNull(),
-  year: integer('year'),
-  artwork_id: integer('artwork_id').references(() => artworks.artwork_id)
+// Tables
+export const artists = pgTable('artists', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar('name', { length: 1024 }).notNull()
 });
 
 export const songs = pgTable('songs', {
-  song_id: integer('song_id').notNull().unique().primaryKey(),
-  title: text('title').notNull(),
-  duration: doublePrecision('duration').notNull().default(0),
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar('title', { length: 4096 }).notNull(),
+  duration: decimal('duration', { precision: 10, scale: 3 }).notNull(),
   path: text('path').notNull().unique(),
-  sample_rate: integer('sample_rate'),
-  bit_rate: integer('bit_rate'),
-  no_of_channels: integer('no_of_channels'),
-  file_created_at: timestamp('file_created_at').notNull(),
-  file_modified_at: timestamp('file_modified_at').notNull(),
-  artwork_id: integer('artwork_id').references(() => artworks.artwork_id),
-  album_id: integer('album_id').references(() => albums.album_id),
-  palette_id: integer('palette_id')
-    .notNull()
-    .references(() => palettes.palette_id),
-  created_at: timestamp('created_at').notNull(),
-  updated_at: timestamp('updated_at').notNull(),
-  deleted_at: timestamp('deleted_at'),
+  sampleRate: integer('sample_rate'),
+  bitRate: integer('bit_rate'),
+  noOfChannels: integer('no_of_channels'),
+  fileCreatedAt: timestamp('file_created_at', { withTimezone: false }).notNull(),
+  fileModifiedAt: timestamp('file_modified_at', { withTimezone: false }).notNull(),
   year: integer('year'),
-  disk_number: integer('disk_number'),
-  track_number: integer('track_number')
+  diskNumber: integer('disk_number'),
+  trackNumber: integer('track_number')
 });
 
-export const artists = pgTable('artists', {
-  artist_id: integer('artist_id').notNull().unique().primaryKey(),
-  name: text('name').notNull().unique(),
-  artwork_id: integer('artwork_id').references(() => artworks.artwork_id),
-  online_artist_artwork_id: integer('online_artist_artwork_id').references(
-    () => onlineArtistArtworks.online_artist_artwork_id
-  )
+export const artworks = pgTable('artworks', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  path: text('path').notNull(),
+  source: artworkSourceEnum('source').notNull().default('LOCAL'),
+  width: integer('width').notNull(),
+  height: integer('height').notNull(),
+  paletteId: integer('palette_id').references(() => palettes.id)
+});
+
+export const palettes = pgTable('palettes', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  songId: integer('song_id')
+    .notNull()
+    .references(() => songs.id)
+});
+
+export const paletteSwatches = pgTable('palette_swatches', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  population: integer('population').notNull(),
+  hex: varchar('hex', { length: 255 }).notNull(),
+  hsl: varchar('hsl', { length: 255 }).notNull(),
+  swatchType: swatchTypeEnum('swatch_type').notNull().default('VIBRANT'),
+  paletteId: integer('palette_id')
+    .notNull()
+    .references(() => palettes.id)
+});
+
+export const albums = pgTable('albums', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar('title', { length: 255 }).notNull(),
+  year: integer('year')
 });
 
 export const genres = pgTable('genres', {
-  genre_id: integer('genre_id').notNull().unique().primaryKey(),
-  name: text('name').notNull().unique(),
-  artwork_id: integer('artwork_id').references(() => artworks.artwork_id),
-  palette_id: integer('palette_id').references(() => palettes.palette_id)
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar('name', { length: 255 }).notNull()
 });
 
 export const playlists = pgTable('playlists', {
-  playlist_id: integer('playlist_id').notNull().unique().primaryKey(),
-  name: text('name').notNull().unique(),
-  artwork_id: integer('artwork_id').references(() => artworks.artwork_id),
-  created_at: timestamp('created_at').notNull().defaultNow()
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar('name', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at', { withTimezone: false }).notNull().defaultNow()
 });
 
-export const listeningData = pgTable('listening_data', {
-  listening_data_id: integer('listening_data_id').notNull().unique().primaryKey(),
-  skip_count: integer('skip_count').notNull().default(0),
-  full_listens_count: integer('full_listens_count').notNull().default(0),
-  added_playlist_count: integer('added_playlist_count').notNull().default(0),
-  song_id: integer('song_id')
+export const playEvents = pgTable('play_events', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  playbackPercentage: decimal('playback_percentage', { precision: 5, scale: 1 }).notNull(),
+  songId: integer('song_id')
     .notNull()
-    .references(() => songs.song_id)
+    .references(() => songs.id),
+  createdAt: timestamp('created_at', { withTimezone: false }).notNull().defaultNow()
+});
+
+export const seekEvents = pgTable('seek_events', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  position: decimal('position', { precision: 8, scale: 3 }).notNull(),
+  songId: integer('song_id')
+    .notNull()
+    .references(() => songs.id),
+  createdAt: timestamp('created_at', { withTimezone: false }).notNull().defaultNow()
+});
+
+export const skipEvents = pgTable('skip_events', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  position: decimal('position', { precision: 8, scale: 3 }).notNull(),
+  songId: integer('song_id')
+    .notNull()
+    .references(() => songs.id),
+  createdAt: timestamp('created_at', { withTimezone: false }).notNull().defaultNow()
 });
 
 export const songBlacklist = pgTable('song_blacklist', {
-  song_blacklist_id: integer('song_blacklist_id').notNull().unique().primaryKey(),
-  song_id: integer('song_id')
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  songId: integer('song_id')
     .notNull()
     .unique()
-    .references(() => songs.song_id)
+    .references(() => songs.id),
+  createdAt: timestamp('created_at', { withTimezone: false }).notNull().defaultNow()
 });
 
-export const userData = pgTable('user_data', {
-  id: integer('id').notNull().unique().primaryKey()
+export const folderBlacklist = pgTable('folder_blacklist', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  path: text('path').notNull().unique()
 });
+
+// Many-to-many linking tables
+export const artworksSongs = pgTable(
+  'artworks_songs',
+  {
+    songId: integer('song_id')
+      .notNull()
+      .references(() => songs.id),
+    artworkId: integer('artwork_id')
+      .notNull()
+      .references(() => artworks.id)
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.songId, table.artworkId] })
+  })
+);
+
+export const artistsArtworks = pgTable(
+  'artists_artworks',
+  {
+    artistId: integer('artist_id')
+      .notNull()
+      .references(() => artists.id),
+    artworkId: integer('artwork_id')
+      .notNull()
+      .references(() => artworks.id)
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.artistId, table.artworkId] })
+  })
+);
+
+export const albumsArtworks = pgTable(
+  'albums_artworks',
+  {
+    albumId: integer('album_id')
+      .notNull()
+      .references(() => albums.id),
+    artworkId: integer('artwork_id')
+      .notNull()
+      .references(() => artworks.id)
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.albumId, table.artworkId] })
+  })
+);
 
 export const artistsSongs = pgTable(
   'artists_songs',
   {
-    song_id: integer('song_id')
+    songId: integer('song_id')
       .notNull()
-      .references(() => songs.song_id),
-    artist_id: integer('artist_id')
+      .references(() => songs.id),
+    artistId: integer('artist_id')
       .notNull()
-      .references(() => artists.artist_id)
+      .references(() => artists.id)
   },
-  (table) => {
-    return {
-      pk: primaryKey({ columns: [table.song_id, table.artist_id] })
-    };
-  }
+  (table) => ({
+    pk: primaryKey({ columns: [table.songId, table.artistId] })
+  })
+);
+
+export const albumSongs = pgTable(
+  'album_songs',
+  {
+    albumId: integer('album_id')
+      .notNull()
+      .references(() => albums.id),
+    songId: integer('song_id')
+      .notNull()
+      .references(() => songs.id)
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.albumId, table.songId] })
+  })
 );
 
 export const genresSongs = pgTable(
   'genres_songs',
   {
-    song_id: integer('song_id')
+    genreId: integer('genre_id')
       .notNull()
-      .references(() => songs.song_id),
-    genre_id: integer('genre_id')
+      .references(() => genres.id),
+    songId: integer('song_id')
       .notNull()
-      .references(() => genres.genre_id)
+      .references(() => songs.id)
   },
-  (table) => {
-    return {
-      pk: primaryKey({ columns: [table.song_id, table.genre_id] })
-    };
-  }
+  (table) => ({
+    pk: primaryKey({ columns: [table.genreId, table.songId] })
+  })
 );
 
-export const albumsArtistsSongs = pgTable(
-  'albums_artists_songs',
+export const artworksGenres = pgTable(
+  'artworks_genres',
   {
-    album_id: integer('album_id')
+    genreId: integer('genre_id')
       .notNull()
-      .references(() => albums.album_id),
-    song_id: integer('song_id')
+      .references(() => genres.id),
+    artworkId: integer('artwork_id')
       .notNull()
-      .references(() => songs.song_id),
-    artist_id: integer('artist_id')
-      .notNull()
-      .references(() => artists.artist_id)
+      .references(() => artworks.id)
   },
-  (table) => {
-    return {
-      pk: primaryKey({ columns: [table.album_id, table.song_id, table.artist_id] })
-    };
-  }
+  (table) => ({
+    pk: primaryKey({ columns: [table.genreId, table.artworkId] })
+  })
 );
-
-export const albumsArtists = pgTable(
-  'albums_artists',
-  {
-    album_id: integer('album_id')
-      .notNull()
-      .references(() => albums.album_id),
-    artist_id: integer('artist_id')
-      .notNull()
-      .references(() => artists.artist_id)
-  },
-  (table) => {
-    return {
-      pk: primaryKey({ columns: [table.album_id, table.artist_id] })
-    };
-  }
-);
-
-export const swatchTypes = pgTable('swatch_types', {
-  swatch_type_id: integer('swatch_type_id').notNull().unique().primaryKey(),
-  name: text('name').notNull().unique()
-});
-
-export const paletteSwatches = pgTable('palette_swatches', {
-  palette_swatch_id: integer('palette_swatch_id').notNull().unique().primaryKey(),
-  population: integer('population').notNull(),
-  hex: text('hex').notNull(),
-  hsl: text('hsl').notNull(),
-  swatch_type_id: integer('swatch_type_id')
-    .notNull()
-    .references(() => swatchTypes.swatch_type_id),
-  palette_id: integer('palette_id')
-    .notNull()
-    .references(() => palettes.palette_id)
-});
-
-export const songSeeks = pgTable('song_seeks', {
-  song_seek_id: integer('song_seek_id').notNull().unique().primaryKey(),
-  position: doublePrecision('position').notNull(),
-  seek_count: integer('seek_count').notNull(),
-  listening_data_id: integer('listening_data_id')
-    .notNull()
-    .references(() => listeningData.listening_data_id)
-});
-
-export const yearlyListens = pgTable('yearly_listens', {
-  yearly_listen_id: integer('yearly_listen_id').notNull().unique().primaryKey(),
-  year: integer('year').notNull(),
-  listening_data_id: integer('listening_data_id')
-    .notNull()
-    .references(() => listeningData.listening_data_id)
-});
-
-export const songListens = pgTable('song_listens', {
-  song_listen_id: integer('song_listen_id').notNull().unique().primaryKey(),
-  listen_count: integer('listen_count').notNull().default(0),
-  listened_at: timestamp('listened_at').notNull().defaultNow(),
-  yearly_listen_id: integer('yearly_listen_id')
-    .notNull()
-    .references(() => yearlyListens.yearly_listen_id)
-});
 
 export const playlistsSongs = pgTable(
   'playlists_songs',
   {
-    playlist_id: integer('playlist_id')
+    playlistId: integer('playlist_id')
       .notNull()
-      .references(() => playlists.playlist_id),
-    song_id: integer('song_id')
+      .references(() => playlists.id),
+    songId: integer('song_id')
       .notNull()
-      .references(() => songs.song_id)
+      .references(() => songs.id)
   },
-  (table) => {
-    return {
-      pk: primaryKey({ columns: [table.playlist_id, table.song_id] })
-    };
-  }
+  (table) => ({
+    pk: primaryKey({ columns: [table.playlistId, table.songId] })
+  })
 );
 
-export const folderBlacklist = pgTable('folder_blacklist', {
-  folder_blacklist_id: integer('folder_blacklist_id').notNull().unique().primaryKey(),
-  folder_path: text('folder_path').notNull().unique()
-});
+export const artworksPlaylists = pgTable(
+  'artworks_playlists',
+  {
+    playlistId: integer('playlist_id')
+      .notNull()
+      .references(() => playlists.id),
+    artworkId: integer('artwork_id')
+      .notNull()
+      .references(() => artworks.id)
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.playlistId, table.artworkId] })
+  })
+);
+
+export const albumArtists = pgTable(
+  'album_artists',
+  {
+    albumId: integer('album_id')
+      .notNull()
+      .references(() => albums.id),
+    artistId: integer('artist_id')
+      .notNull()
+      .references(() => artists.id)
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.albumId, table.artistId] })
+  })
+);
 
