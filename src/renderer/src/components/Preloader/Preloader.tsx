545be58385ce495bc73cd33fe9c1a234b814f7cd
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2025-10-10 23:07:52 +0530

    refactor: comment out preloader logic and enhance route handling with loader and pending components

--- src/renderer/src/components/Preloader/Preloader.tsx
+++ src/renderer/src/components/Preloader/Preloader.tsx
@@ -1,33 +1,33 @@
-import { useEffect } from 'react';
-import { router } from '@renderer/index';
+// import { useEffect } from 'react';
+// import { router } from '@renderer/index';
 
 import Img from '../Img';
 
 import AppIcon from '../../assets/images/webp/logo_light_mode.webp';
 
-const contentLoadStart = window.performance.now();
-const hidePreloader = () => router.navigate({ to: '/main-player/home', replace: true });
+// const contentLoadStart = window.performance.now();
+// const hidePreloader = () => router.navigate({ to: '/main-player/home', replace: true });
 
-window.addEventListener(
-  'load',
-  () => {
-    setTimeout(() => hidePreloader(), 1000);
+// window.addEventListener(
+//   'load',
+//   () => {
+//     setTimeout(() => hidePreloader(), 1000);
 
-    console.warn('contentLoad', window.performance.now() - contentLoadStart, document.readyState);
-  },
-  { once: true }
-);
+//     console.warn('contentLoad', window.performance.now() - contentLoadStart, document.readyState);
+//   },
+//   { once: true }
+// );
 
 const Preloader = () => {
-  useEffect(() => {
-    router.preloadRoute({ to: '/main-player/home' });
-    // this removes preloader in 5 seconds no matter what didn't load.
-    const timeoutId = setTimeout(() => hidePreloader(), 2000);
+  // useEffect(() => {
+  //   router.preloadRoute({ to: '/main-player/home' });
+  //   // this removes preloader in 5 seconds no matter what didn't load.
+  //   const timeoutId = setTimeout(() => hidePreloader(), 2000);
 
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
+  //   return () => {
+  //     clearTimeout(timeoutId);
+  //   };
+  // }, []);
 
   return (
     <div className="preloader bg-background-color-1 dark:bg-dark-background-color-1 visible absolute z-40 flex h-full w-full items-center justify-center opacity-100 transition-[visibility,opacity] delay-700">
--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,10 +1,23 @@
 import Preloader from '@renderer/components/Preloader/Preloader';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Navigate } from '@tanstack/react-router';
+import { queryClient, router } from '..';
+import { settingsQuery } from '@renderer/queries/settings';
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent
+  component: RouteComponent,
+  pendingComponent: () => <Preloader />,
+  loader: async () => {
+    await Promise.all([
+      queryClient.ensureQueryData(settingsQuery.all),
+      router.preloadRoute({ to: '/main-player/home' })
+    ]);
+  },
+  // override pendingMs to 0 to show preloader immediately
+  pendingMs: 0,
+  // ensure preloader shows for at least 1000ms
+  pendingMinMs: 1000
 });
 
 function RouteComponent() {
-  return <Preloader />;
+  return <Navigate to="/main-player/home" />;
 }
