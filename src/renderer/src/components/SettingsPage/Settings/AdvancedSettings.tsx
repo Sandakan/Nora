a93374fd6c290066de28d536d79f4eec91ff6562
Abbas Sadeghi <abbas.sadeghi1384@gmail.com>
2025-11-23 23:23:55 +0330

    feat: make AdvancedSettings section collapsible

--- src/renderer/src/components/SettingsPage/Settings/AdvancedSettings.tsx
+++ src/renderer/src/components/SettingsPage/Settings/AdvancedSettings.tsx
@@ -3,6 +3,7 @@ import Checkbox from '../../Checkbox';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { settingsQuery } from '@renderer/queries/settings';
 import { queryClient } from '@renderer/index';
+import CollapsibleSection from "./CollapsibleSection";
 
 const AdvancedSettings = () => {
   const { data: userSettings } = useQuery(settingsQuery.all);
@@ -18,13 +19,18 @@ const AdvancedSettings = () => {
 
   return (
     <li
-      className="main-container performance-settings-container mb-16"
+      className="main-container performance-settings-container mb-4"
       id="advanced-settings-container"
     >
-      <div className="title-container text-font-color-highlight dark:text-dark-font-color-highlight mt-1 mb-4 flex items-center text-2xl font-medium">
+    <CollapsibleSection
+      defaultOpen={false}
+      title={
+      <div className="title-container text-font-color-highlight dark:text-dark-font-color-highlight flex items-center text-2xl font-medium">
         <span className="material-icons-round-outlined mr-2 leading-none">handyman</span>
         <span> {t('settingsPage.advanced')}</span>
       </div>
+      }
+    >
       <ul className="marker:bg-background-color-3 dark:marker:bg-background-color-3 list-disc pl-6">
         <li className="secondary-container toggle-save-verbose-logs mb-4">
           <div className="description">{t('settingsPage.saveVerboseLogsDescription')}</div>
@@ -36,6 +42,7 @@ const AdvancedSettings = () => {
           />
         </li>
       </ul>
+    </CollapsibleSection>
     </li>
   );
 };
